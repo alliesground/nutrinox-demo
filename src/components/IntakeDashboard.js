8acebe77938e34b0cd19e65feae3caec65ca6e57
Sameer Subba <sameer.libang@gmail.com>
2019-09-08 14:36:27 +1000

    moving intake sliding responsibility to dashboard

--- src/components/IntakeDashboard.js
+++ src/components/IntakeDashboard.js
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Grid } from 'semantic-ui-react';
+import { useData } from './hooks/useData'
 import FoodSearchForm from './FoodSearchForm';
-import SlidableIntakeList from './SlidableIntakeList';
+import IntakeListSlider from './IntakeListSlider';
+import IntakeList from './IntakeList'
 
-const IntakeDashboard = () =>(
-  <>
-    <FoodSearchForm />
-    <SlidableIntakeList />
-  </>
-)
+const IntakeDashboard = () => {
+  const [diet, setDiet] = useData();
+
+  const [currentDataPointIndex, setCurrentDataPointIndex] = useState(0)
+
+  const currentDate = diet.data_points[currentDataPointIndex].date;
+  
+  const initialIntakeList = diet.data_points[currentDataPointIndex].intake_list
+
+  const [date, setDate] = useState(currentDate);
+
+  const [intakeList, setIntakeList] = useState(initialIntakeList)
+
+  const handlePrevClick = () => {
+    if (currentDataPointIndex >= diet.data_points.length - 1) return
+
+    setCurrentDataPointIndex(
+      currentDataPointIndex + 1
+    )
+  }
+
+  const handleNextClick = () => {
+    if (currentDataPointIndex <= 0) return
+
+    setCurrentDataPointIndex(
+      currentDataPointIndex - 1
+    )
+  }
+
+  useEffect(() => {
+    setDate(diet.data_points[currentDataPointIndex].date)
+    setIntakeList(diet.data_points[currentDataPointIndex].intake_list)
+  }, [currentDataPointIndex])
+
+  return (
+    <>
+      <FoodSearchForm />
+      <IntakeListSlider 
+        onPrevClick={handlePrevClick}
+        onNextClick={handleNextClick}
+        date={date}
+      />
+      <IntakeList 
+        intakeList={intakeList}
+      />
+    </>
+  )
+}
 
 export default IntakeDashboard;
 
--- src/components/IntakeList.js
+++ src/components/IntakeList.js
@@ -1,17 +1,20 @@
 import React from 'react'
+import { Grid } from 'semantic-ui-react'
 
 const IntakeList = ({ intakeList }) => {
   console.log('List: ', intakeList)
   return (
-    <div>
-      {
-        intakeList.map(intake => (
-          <div>
-            {intake.food_name}
-          </div>
-        ))
-      }
-    </div>
+    <Grid doubling centered columns={4}>
+      <Grid.Column>
+        {
+          intakeList.map(intake => (
+            <div>
+              {intake.food_name}
+            </div>
+          ))
+        }
+      </Grid.Column>
+    </Grid>
   )
 }
 

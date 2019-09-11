7415299fd90e3f6955ca9f56b4dea5fdf1ddbf22
Sameer Subba <sameer.libang@gmail.com>
2019-09-11 21:06:46 +1000

    styling computer version of user profile

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,47 +1,61 @@
 import React from 'react'
-import { Grid, Header } from 'semantic-ui-react';
+import { Container, Grid, Header, Image, Label } from 'semantic-ui-react';
 
-const info = () => (
-  <Grid columns={3}>
-    <Grid.Column>
-      1col
+const computerLayout = () => (
+  <>
+    <Grid.Column verticalAlign='middle' textAlign='right'>
+      <Label circular size='massive'>
+        <Header as='h3'>
+          153
+          <Header.Subheader>Kg</Header.Subheader>
+        </Header>
+      </Label>
     </Grid.Column>
 
     <Grid.Column>
-      2col
+      <Image 
+        src='https://d1r9wva3zcpswd.cloudfront.net/55c92acdf04322593691010c.jpeg' 
+        size='medium' circular 
+      />
     </Grid.Column>
 
-    <Grid.Column>
-      3col
+    <Grid.Column verticalAlign='middle'>
+      <Label circular size='massive'>
+        <Header as='h3'>
+          153
+          <Header.Subheader>Kg</Header.Subheader>
+        </Header>
+      </Label>
     </Grid.Column>
     
-    <Header>Jane Appleseed</Header>
-  </Grid>
+    <Container 
+      fluid 
+      textAlign='center'
+      style={{
+        padding: '20px',
+        paddingBottom: '0px'
+      }}
+    >
+      <Header as='h2'>Jane Appleseed</Header>
+    </Container>
+  </>
 )
 
-const intakeInfo = () => (
-  <Grid.Column>
-    <Grid>
-      <Grid.Row>
-        <Grid.Column>
-          <Header>More Info</Header>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
-  </Grid.Column>
+const mobileLayout = () => (
+  <>
+    coming soon...
+  </>
 )
 
 const UserProfile = () => {
   return (
     <Grid>
-      <Grid.Row>
-        <Grid.Column>
-          { info() }
-        </Grid.Column>
+      <Grid.Row only='large screen computer tablet' columns={3}>
+        { computerLayout() }
       </Grid.Row>
 
-      <Grid.Row only='large screen computer tablet'>
-        { intakeInfo() }
+      <Grid.Row only='mobile'>
+        { mobileLayout() }
       </Grid.Row>
     </Grid>
   )
